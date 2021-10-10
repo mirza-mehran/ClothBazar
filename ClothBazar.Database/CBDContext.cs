0ad1b68faf7c26435c3df153c789edaa741a25ee
Mirza_Mehran <Mirza_Mehran@DESKTOP-MSO8RDH>
2021-10-10 14:32:01 +0500

    Added Product and Category Business Entity and Created Database.

---
+++ ClothBazar.Database/CBDContext.cs
@@ -0,0 +1,30 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Database
+{
+    public  class CBDContext : DbContext
+    {
+        public CBDContext():base("ClothBazarCon")
+        {
+          //  System.Data.Entity.Database.SetInitializer(new MigrateDatabaseToLatestVersion<CBDContext, Migrations.Configuration>());
+        }
+        public DbSet<Product> Products
+        {
+            get; set;
+        }
+        public DbSet<Category> Categories
+        {
+            get; set;
+        }
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+
+        //}
+    }
+}
