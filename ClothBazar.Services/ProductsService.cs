3915420054ffc2b60e7ab43012e2ad2470b676bf
Mirza_Mehran <Mirza_Mehran@DESKTOP-MSO8RDH>
2021-10-14 02:38:00 +0500

    Apply Search TextBox With JQUERY

---
+++ ClothBazar.Services/ProductsService.cs
@@ -0,0 +1,55 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+   public  class ProductsService
+    {
+        public void SaveProduct(Product product)
+        {
+            using (var context=new CBDContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public List<Product> GetAllProduct()
+        {
+            using (var context = new CBDContext())
+            {               
+                return  context.Products.ToList();
+            }
+        }
+
+        public Product GetProductID(int id)
+        {
+            using (var context = new CBDContext())
+            {
+               return  context.Products.Find(id);            
+            }
+        }
+
+        public void GetProductUpdate(Product product)
+        {
+            using (var context = new CBDContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void GetProductDelete(int id)
+        {
+            using (var context = new CBDContext())
+            {
+                var product = context.Products.Find(id);
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ClothBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductList(string search)
+        {
+            var data = productsService.GetAllProduct();
+            if (!string.IsNullOrEmpty(search))
+            {
+                data = data.Where(x => x.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+            return PartialView(data);
+        }
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                productsService.SaveProduct(product);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
